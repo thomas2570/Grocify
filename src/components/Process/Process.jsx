004c7108803395206b8be7c5105a696d9286308d
thomas2570 <thomasramesh449@gmail.com>
2026-07-30 23:53:39 +0530

    commit: Refactor Process component to render steps dynamically

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -6,14 +6,39 @@ import { SlBadge } from "react-icons/sl";
 import { BsTruck } from "react-icons/bs";
 
 const Process = () => {
+
+  const renderSteps = steps.map(item=>{
+    return(
+      <div>
+        <span>{item.number}</span>
+        <div>
+
+          <div>
+            <span>{item.icon}</span>
+          </div>
+
+          <div>
+            <h4>{item.title}</h4>
+            <p>{item.para}</p>
+          </div>
+        </div>
+      </div>
+    )
+  })
+
+
   return (
     <section>
-        <div className='max-w-[1400px] mx-auto px-10 py-20'>
-            <div className='w-fit mr-auto'>
-              <Heading highlight="Our" heading="Process"/>
-            </div>
-
+      <div className='max-w-[1400px] mx-auto px-10 py-20'>
+        <div className='w-fit mr-auto'>
+          <Heading highlight="Our" heading="Process" />
         </div>
+
+        <div>
+           {renderSteps}
+        </div>
+
+      </div>
     </section>
   )
 }
@@ -28,21 +53,21 @@ const steps = [
     para: 'It is a long established fact that a reader',
     icon: <PiPlant />,
   },
-    {
+  {
     id: 2,
     number: <TbCircleNumber2Filled />,
     title: 'Manufacturing',
     para: 'It is a long established fact that a reader',
     icon: <PiFactory />,
   },
-    {
+  {
     id: 3,
     number: <TbCircleNumber3Filled />,
     title: 'Quality Control',
     para: 'It is a long established fact that a reader',
     icon: <SlBadge />,
   },
-    {
+  {
     id: 4,
     number: <TbCircleNumber4Filled />,
     title: 'Logistics',

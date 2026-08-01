ae52306ac9fdc68211eec4aab767894cfbeb097a
thomas2570 <thomasramesh449@gmail.com>
2026-08-01 13:12:37 +0530

    commit message: Update Process component layout and styling

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -9,17 +9,15 @@ const Process = () => {
 
   const renderSteps = steps.map(item=>{
     return(
-      <div>
-        <span>{item.number}</span>
-        <div>
+      <div className={`flex-1 basis-[300px] ${item.id % 2 === 0 ? 'md:-mt-100' : ''}`}>
+        <span className='flex justify-center items-center w-18 h-18 mx-auto rounded-full text-8xl text-zinc-800 text-white outline-[3px] outline-offset-7 outline-zinc-800 outline-dashed'>{item.number}</span>
 
-          <div >
-            <span>{item.icon}</span>
-          </div>
-
-          <div>
-            <h4>{item.title}</h4>
-            <p>{item.para}</p>
+        <div className='flex items-center gap-x-5 mt-10'>
+            <span className='flex justify-center items-center text-3xl bg-gradient-to-b from-orange-400 to-orange-500 text-white w-15 h-15 rounded-full'>{item.icon}</span>
+          
+          <div className='flex-1'>
+            <h4 className='text-zinc-800 text-2xl font-bold'>{item.title}</h4>
+            <p className='text-zinc-600 mt-2'>{item.para}</p>
           </div>
         </div>
       </div>
@@ -34,7 +32,7 @@ const Process = () => {
           <Heading highlight="Our" heading="Process" />
         </div>
 
-        <div className='flex mt-20'>
+        <div className='flex flex-wrap gap-y-17 items-center justify-center md:mt-20 mt-10 md:pt-50'>
            {renderSteps}
         </div>
 
